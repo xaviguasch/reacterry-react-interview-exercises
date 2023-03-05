d7d24a00b9f75ed3293d51baa54a36a21ab274e7
xaviguasch <xaviguasch@gmail.com>
2023-03-05 16:54:14 +0100

    added r01-counter--EASY challenge

---
+++ r01-counter-reacterry-exercise-app--EASY/src/App.js
@@ -0,0 +1,20 @@
+import './App.css'
+
+// Counter
+// Write a component called App. Add two buttons, one with the text Increment and the other with the text Decrement. This component should track the difference between how many times the Increment vs Decrement buttons were clicked. Display the current count to the page in an h1 element. The updated value should be displayed to the user immediately.
+
+// Directions
+// Return a button with the text Increment
+// Return a button with the text Decrement
+// Return the current count in h1 JSX element
+
+function App() {
+  return (
+    <div>
+      <h1>Counter</h1>
+      <button>Incgrement</button>
+    </div>
+  )
+}
+
+export default App
