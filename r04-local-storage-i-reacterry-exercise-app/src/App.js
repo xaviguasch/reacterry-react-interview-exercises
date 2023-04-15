b307956f847a8b87b5d30465402e516144f9a5fe
Xavi Guasch <xaviguasch@gmail.com>
2023-04-15 16:38:38 +0200

    added solution to previous challenge, with one pending test

--- r04-local-storage-i-reacterry-exercise-app/src/App.js
+++ r04-local-storage-i-reacterry-exercise-app/src/App.js
@@ -1,12 +1,26 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 
 const App = () => {
   // Edit this component
+  const [text, setText] = useState(() => localStorage.getItem('inputValue') || '')
+
+  useEffect(() => {
+    localStorage.setItem('inputValue', text)
+  }, [text])
+
   return (
     <div>
-      <input data-testid='input-id' type='text' />
+      <input
+        data-testid='input-id'
+        type='text'
+        value={text}
+        onChange={(e) => setText(e.target.value)}
+      />
     </div>
   )
 }
 
 export default App
+
+// Doubt with a small test that doesn't pass
+// ✕ Does not update the local storage when the input field is empty(6 ms)
